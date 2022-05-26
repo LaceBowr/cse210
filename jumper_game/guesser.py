a33a9b67af32f63fa2c48a485c186cd90457ec6c
Fireguy13 <104537058+Fireguy13@users.noreply.github.com>
2022-05-25 18:03:56 -0600

    guesser.py
    this what i did

--- jumper_game/guesser.py
+++ jumper_game/guesser.py
@@ -1,38 +1,23 @@
 import random
 
-class Guesser:
-    """The person guessing letters in the word selected from the computer at random. 
-
-    The responsibility of Guesser is to collect input and validate if guesses are RIGHT or WRONG. 
-
-    Attributes:
-        check_guess (True=RIGHT or False=WRONG): The letter selected between (A-Z).
-        _random_word (auto_generated): The letters that can end up correctly guessed.
-    """
-
+class Guesser: 
+    words = ("tree","mars","class", "scissor", "papper", "rock", "fire" )
     def __init__(self):
-        """Constructs a new Guess.
+        self.random = ''
 
-        Args:
-            self (Guess): A new instance of Jumper.
-        """
-        # the random word we will be guessing
-        self._random_word = ""
+    def get_ramdom_word(self):
+        self.random = self.get_word()
 
-        # The current guessed state of the random word above
-        # for testing I have set it to random half guessed word
-        self._word_with_placeholders = "A_la__"
+    def get_word(self):
+        word = random.choice(self.words)
+        return word
 
-    # A random word is generated and stores it in the guesser class that will be used thoughout the game.
-    def random_word(self):
-        # TODO
-        return
+    def guess(self,letter):
+        indices_of_letter = []
+        for i, ch in enumerate(self.random):
+            if ch == letter:
+                indices_of_letter.append(i)
+        temp = indices_of_letter
+        return temp
 
-    def check_guess(self, player_move):
-        # TODO
-        # checks the player_move and returns true if it is in the word, false otherwise.
-        # and updates the current state of _word_with_placeholders
-        return True
 
-    def get_word_with_placeholders(self):
-        return self._word_with_placeholders
