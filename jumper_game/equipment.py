9076674b4a98e51c79e96e9fd405248722fcdfee
Webdeveloperintraining <103979055+Webdeveloperintraining@users.noreply.github.com>
2022-05-25 18:42:15 -0700

    Update equipment.py
    This is my part of the code

--- jumper_game/equipment.py
+++ jumper_game/equipment.py
@@ -1,18 +1,35 @@
-
+from guesser import Guesser
+from jumper_game.jumperdirector import Director
+ 
 class Equipment(object):
     def __init__(self):
-        self.jumper_details = [[' ',' ','_','_','_'],
-            [' ','/','_','_','_','\\'],
-            [' ','\\',' ',' ',' ','/'],
-            [' ',' ','\\',' ','/',],
-            [' ',' ',' ','0'],
-            [' ',' ','/','|','\\'],
-            [' ',' ','/',' ','\\ ',' ',' ',]]
-
+        self.parachute="   _____ \n"+"  /_____\ \n"+"  \     / \n"+"   \   / "
+        self.skydiver="     0 \n"+"    /|\ \n"+"    / \ "
+        get_word=Guesser()
+        player_move=Director()
+        self.move=player_move._input_administrator()
+        self._word=""
+        self._word=get_word.get_word_with_placeholders()
+        self.letters=get_word.guess()
+ 
     def blast_parachute(self):
-        # TODO
         # removes a line from the jumper
-        pass
-    
-    def get_jumper(self):
-        return self.jumper_details
+        n=0
+        while n != 5:
+            if self._word not in self.letters:
+                parachute=self.parachute[10:]
+                return parachute
+            if n >= 4:
+                skydiver=skydiver.replace("     0 \n","     X \n")
+                print(skydiver)
+                print("gameover")
+            return skydiver
+ 
+    def parachute_is_safe(self,word):
+ 
+        for letter in word:
+            if letter.lower() == True:
+                print(letter,end= " ")
+            else:
+                print("_",end=" ")
+   
