699115d2047cb2970f2c97ce2aac0f071c880599
LaceBowr <97077997+LaceBowr@users.noreply.github.com>
2022-05-25 20:25:30 -0700

    guesser.py

--- jumper_game/guesser.py
+++ jumper_game/guesser.py
@@ -21,17 +21,29 @@ class Guesser:
 
         # The current guessed state of the random word above
         # for testing I have set it to random half guessed word
-        self._word_with_placeholders = "A_la__"
+        self._word_with_placeholders = " "
+
+    def get_word(self):
+        words = ['hello']
+        return random.choice(words)
 
     # A random word is generated and stores it in the guesser class that will be used thoughout the game.
     def random_word(self):
-        # TODO
+        self._random_word = self.get_word()
         return
 
+    def get_random_word(self):
+        return self._random_word
+
     def check_guess(self, player_move):
-        # TODO
+        
         # checks the player_move and returns true if it is in the word, false otherwise.
         # and updates the current state of _word_with_placeholders
+        indices_of_letter = []
+        for i, ch in enumerate(self._random_word):
+            if ch == player_move:
+                indices_of_letter.append(i)
+        temp = indices_of_letter
         return True
 
     def get_word_with_placeholders(self):
