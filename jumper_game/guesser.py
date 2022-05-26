2ac26099f1c08db423684f73ea67a1523ea58dad
LaceBowr <97077997+LaceBowr@users.noreply.github.com>
2022-05-25 20:15:35 -0700

    Update guesser.py

--- jumper_game/guesser.py
+++ jumper_game/guesser.py
@@ -5,19 +5,23 @@ class Guesser:
     def __init__(self):
         self.random = ''
 
-    def get_ramdom_word(self):
+    def get_random_word(self):
         self.random = self.get_word()
+        return word
 
     def get_word(self):
         word = random.choice(self.words)
         return word
 
-    def guess(self,letter):
+    def check_guess(self, player_move):
         indices_of_letter = []
         for i, ch in enumerate(self.random):
-            if ch == letter:
+            if ch == player_move:
                 indices_of_letter.append(i)
         temp = indices_of_letter
-        return temp
+        return True
+    
+    def get_word_with_placeholders(self):
+        return self._word_with_placeholders
 
 
